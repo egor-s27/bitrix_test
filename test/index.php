124a4d06f8bbe9e5ce8a54b8b38fe9dad9a5782e
Смолин Егор Андреевич <egor.smolin@omnichannel.ru>
2024-10-17 17:06:55 +0400

    Initial commit

---
+++ test/index.php
@@ -0,0 +1,28 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Главная");
+?>
+    <pre>
+<?php
+if (CModule::IncludeModule("iblock")) {
+    //здесь можно использовать функции и классы модуля
+
+    $arSelect = array("ID", "NAME");//IBLOCK_ID и ID обязательно должны быть указаны, см. описание arSelectFields выше
+    $arFilter = array("IBLOCK_ID" => 5, "ACTIVE_DATE" => "Y", "ACTIVE" => "Y");
+    $res = CIBlockElement::GetList(array(), $arFilter, false, array("nPageSize" => 50), $arSelect);
+    $arBlock = GetIBlock("3");
+
+    while ($ob = $res->Fetch()) {
+        $arItem = $ob->GetFields();
+        $arResult[$arItem["ID"]] = $arItem["NAME"];
+    }
+}
+?>
+    </pre>
+    <h1><?= $arBlock["NAME"] ?></h1>
+    <ul>
+        <?php foreach ($arResult as $id => $arElement): ?>
+            <li><?= $id ?> - <?= $arElement ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
